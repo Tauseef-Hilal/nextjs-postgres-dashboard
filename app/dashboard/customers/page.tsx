612b9413f4daf7a133a519c8fd4dfc226e9e7149
Tauseef-Hilal <tantary.tauseef.atwork@gmail.com>
2023-11-11 08:22:30 +0530

    Finish course

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,21 @@
+import Table from "@/app/ui/customers/table";
 import { Metadata } from "next";
+import { fetchFilteredCustomers } from "@/app/lib/data";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { query: string };
+}) {
+  const customers = await fetchFilteredCustomers(searchParams.query ?? '');
+
+  return (
+    <div className="w-full">
+      <Table customers={customers} />
+    </div>
+  );
 }
